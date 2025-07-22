b972c351d40b3b362cd4d97606d1ec2010e01079
httpsMansoor <malangbaba535@gmail.com>
2025-07-23 01:25:01 +0500

    fix static files not lading 2

--- TechHive/settings.py
+++ TechHive/settings.py
@@ -76,24 +76,15 @@ TEMPLATES = [
 # ======================
 # DATABASE CONFIGURATION
 # ======================
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'techhive',
-        'USER': 'admin',
-        'PASSWORD': 'admin123',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+
 # To switch to production DB:
-# DATABASES = {
-#     'default': dj_database_url.config(
-#         default=os.getenv('DATABASE_URL'),
-#         conn_max_age=600,
-#         ssl_require=not DEBUG
-#     )
-# }
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=not DEBUG
+    )
+}
 
 # ======================
 # PASSWORD VALIDATION
