7d743614ce740a0a8557b43b2ca8b63017c2cc83
httpsMansoor <malangbaba535@gmail.com>
2025-07-23 21:23:25 +0500

    fixed images not loading n production

--- TechHive/urls.py
+++ TechHive/urls.py
@@ -77,6 +77,5 @@ urlpatterns = [
     path('', include(docs_urlpatterns)),
 ]
 
-# Serve media files in development
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
