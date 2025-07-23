a7d5806d1bdd73f6cb85eb8c30c456da6acb9f51
httpsMansoor <malangbaba535@gmail.com>
2025-07-23 22:27:09 +0500

    fixed image field mistakenly typed in models

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
@@ -16,7 +17,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     stock = models.PositiveIntegerField(default=0)
-    image = models.ImageField(upload_to='products/', blank=True, null=True)
+    image = CloudinaryField('image', blank=True, null=True) 
     average_rating = models.FloatField(default=0.0)
     review_count = models.PositiveIntegerField(default=0)
     positive_reviews = models.PositiveIntegerField(default=0)
