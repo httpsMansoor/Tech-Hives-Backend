f408d41b66106b8c9a8250216e13bd7d672adf57
httpsMansoor <malangbaba535@gmail.com>
2025-07-25 21:22:06 +0500

    Fixed review serializer to return the auther name and email

--- reviews/serializers.py
+++ reviews/serializers.py
@@ -2,7 +2,13 @@ from rest_framework import serializers
 from .models import Review
 from products.serializers import ProductSerializer
 
+from rest_framework import serializers
+from .models import Review
+from products.serializers import ProductSerializer
+
+
 class ReviewSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField()  # This will show user's email or __str__ from CustomUser
     user_full_name = serializers.SerializerMethodField()
     product = ProductSerializer(read_only=True)
     sentiment = serializers.SerializerMethodField()
@@ -11,7 +17,8 @@ class ReviewSerializer(serializers.ModelSerializer):
         model = Review
         fields = [
             'id',
-            'user_full_name',
+            'user',             # ← this line shows user __str__ (usually email)
+            'user_full_name',   # ← this is full_name from your CustomUser model
             'product',
             'rating',
             'comment',
@@ -19,10 +26,9 @@ class ReviewSerializer(serializers.ModelSerializer):
             'sentiment_score',
             'sentiment'
         ]
-        read_only_fields = ['id', 'created_at', 'sentiment_score']
+        read_only_fields = ['id', 'user', 'created_at', 'sentiment_score']
 
     def get_user_full_name(self, obj):
-        # Return full name from related user model
         return obj.user.full_name if obj.user.full_name else obj.user.email
 
     def get_sentiment(self, obj):
@@ -34,6 +40,7 @@ class ReviewSerializer(serializers.ModelSerializer):
         if value < 1 or value > 5:
             raise serializers.ValidationError("Rating must be between 1 and 5")
         return value
+
 
 class CreateReviewSerializer(serializers.ModelSerializer):
     class Meta:
